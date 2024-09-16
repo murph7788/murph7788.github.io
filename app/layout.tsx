9dfb42baadf48c9009402431171139526bc80162
murph <murphyxiaoxi@outlook.com>
2024-09-16 21:31:21 +0800

    google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,6 +90,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         href={`${basePath}/static/favicons/safari-pinned-tab.svg`}
         color="#5bbad5"
       />
+      <meta name="google-site-verification" content="zvFYEMc4xwg0mhb00KQbNAqQw-RUqqH59GH-N9AJ8Cg" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
